f85434ccd96dc8cfcbb42ed1c1ba60a575bae675
mohamedamr77 <moamr947@gmail.com>
2025-01-06 04:18:18 +0200

    32

--- lib/features/favourite_items/presentation/view/widget/favourite_body.dart
+++ lib/features/favourite_items/presentation/view/widget/favourite_body.dart
@@ -4,6 +4,7 @@ import 'package:gradutionproject/core/navigation/navigation_manager.dart';
 import 'package:gradutionproject/core/shared_widget/global_text.dart';
 import 'package:gradutionproject/core/utils/app_colors.dart';
 import 'package:gradutionproject/core/utils/app_icons.dart';
+import 'package:gradutionproject/core/utils/app_images.dart';
 import 'package:gradutionproject/core/utils/app_text.dart';
 import 'package:gradutionproject/core/utils/extentions/screen_size.dart';
 import 'package:gradutionproject/features/favourite_items/presentation/viewModel/favouite_item_state.dart';
@@ -15,7 +16,7 @@ class FavouriteBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var cubit =BlocProvider.of<FavouriteItemsCubit>(context);
+    var cubit = BlocProvider.of<FavouriteItemsCubit>(context);
     return BlocBuilder<FavouriteItemsCubit, FavouriteItemsState>(
       builder: (context, state) {
         return Padding(
@@ -24,13 +25,13 @@ class FavouriteBody extends StatelessWidget {
               slivers: [
                 const SliverToBoxAdapter(
                     child: SizedBox(
-                      height: 16,
-                    )),
+                  height: 16,
+                )),
                 SliverToBoxAdapter(
                   child: SizedBox(
                     child: SingleChildScrollView(
                         scrollDirection:
-                        Axis.horizontal, // Enable horizontal scrolling
+                            Axis.horizontal, // Enable horizontal scrolling
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: List.generate(2, (index) {
@@ -38,49 +39,48 @@ class FavouriteBody extends StatelessWidget {
                               padding: const EdgeInsets.symmetric(
                                   horizontal: 8), // Add padding between items
                               child: GestureDetector(
-                                onTap: () {
-                                  cubit.selectButton(index);
-                                },
-                                child:
-                                index ==cubit.buttonSelected?
-                                Container(
-                                  width: 0.42.w,
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 12),
-                                  decoration: BoxDecoration(
-                                    color: AppColors.primaryColor,
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  child: const Center(
-                                    child: GText(
-                                      color: AppColors.whiteColor,
-                                      content: AppText.doctors,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ) :
-                                Container(
-                                  width: 0.42.w,
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 12),
-                                  decoration: BoxDecoration(
-                                    color: AppColors.secondaryColor,
-                                    borderRadius: BorderRadius.circular(20),
-                                    border: Border.all(
-                                      color: AppColors.primaryColor
-                                    )
-                                  ),
-                                  child: const Center(
-                                    child: GText(
-                                      color: AppColors.primaryColor,
-                                      content: AppText.doctors,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                )
-                              ),
+                                  onTap: () {
+                                    cubit.selectButton(index);
+                                  },
+                                  child: index == cubit.buttonSelected
+                                      ? Container(
+                                          width: 0.42.w,
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 12),
+                                          decoration: BoxDecoration(
+                                            color: AppColors.primaryColor,
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                          ),
+                                          child: const Center(
+                                            child: GText(
+                                              color: AppColors.whiteColor,
+                                              content: AppText.doctors,
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ),
+                                        )
+                                      : Container(
+                                          width: 0.42.w,
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 12),
+                                          decoration: BoxDecoration(
+                                              color: AppColors.secondaryColor,
+                                              borderRadius:
+                                                  BorderRadius.circular(20),
+                                              border: Border.all(
+                                                  color:
+                                                      AppColors.primaryColor)),
+                                          child: const Center(
+                                            child: GText(
+                                              color: AppColors.primaryColor,
+                                              content: AppText.doctors,
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ),
+                                        )),
                             );
                           }),
                         )),
@@ -88,21 +88,85 @@ class FavouriteBody extends StatelessWidget {
                 ),
                 const SliverToBoxAdapter(
                     child: SizedBox(
-                      height: 16,
-                    )),
+                  height: 16,
+                )),
                 SliverList.separated(
                   itemCount: 10,
                   itemBuilder: (context, index) {
                     return Container(
-                      height: 200,
                       width: double.infinity,
-                      color: Colors.redAccent,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: AppColors.secondaryColor,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 12),
+                        child: Row(
+                          textDirection: TextDirection.rtl,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Image(
+                              image: const AssetImage(AppImages.tuberVaccineTest),
+                              width: 0.28.w,
+                              height: 0.12.h,
+                              fit: BoxFit.fill,
+                            ),
+                            const SizedBox(
+                              width: 14,
+                            ),
+                             Column(
+                               mainAxisAlignment: MainAxisAlignment.start,
+                               crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                SizedBox(
+                                  width: 0.5.w,
+                                child: const GText(
+                                  color: AppColors.blackColor,
+                                  content:
+                                  AppText.sideEffectsTuberculosisVaccine,
+                                  fontSize: 16,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                ),
+                                const SizedBox(height: 8,),
+                                SizedBox(
+                                  width: 0.5.w,
+                                child: const GText(
+                                  color: AppColors.primaryColor,
+                                  content:
+                                 "فعال بنسبة99%",
+                                  fontSize: 14,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                                ),
+                                const SizedBox(height: 4,),
+                                 const GText(
+                                  textAlign: TextAlign.right,
+                                  color: AppColors.grayColor1,
+                                  content:
+                                  "يتم اخده مره واحده",
+                                  fontSize: 14,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
                     );
-                  }, separatorBuilder: (BuildContext context, int index) {
-                  return SizedBox(
-                    height: 8,
-                  );
-                },
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const SizedBox(
+                      height: 8,
+                    );
+                  },
                 )
               ],
             ));
