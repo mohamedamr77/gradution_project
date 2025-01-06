0d796f7a677fd25499100d548e5647eba15aff15
mohamedamr77 <moamr947@gmail.com>
2025-01-06 06:25:26 +0200

    refecator code

--- lib/features/favourite_items/presentation/view/widget/favourite_body.dart
+++ lib/features/favourite_items/presentation/view/widget/favourite_body.dart
@@ -15,90 +15,89 @@ class FavouriteBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var cubit = BlocProvider.of<FavouriteItemsCubit>(context);
+
     return BlocBuilder<FavouriteItemsCubit, FavouriteItemsState>(
       builder: (context, state) {
         return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 0.04.w, vertical: 0.02.h),
-            child: CustomScrollView(
-              slivers: [
-                const SliverToBoxAdapter(
-                    child: SizedBox(
-                  height: 16,
-                )),
-                SliverToBoxAdapter(
-                  child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: List.generate(buttonFavouriteTitles.length, (index) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8),
-                            child: GestureDetector(
-                                onTap: () {
-                                  cubit.selectButton(index);
-                                },
-                                child: Container(
-                                  width: 0.42.w,
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 12),
-                                  decoration: BoxDecoration(
-                                    color: index == cubit.buttonSelected ?
-                                    AppColors.primaryColor :
-                                    AppColors.secondaryColor,
-                                      border: Border.all(
-                                          color:
-                                          AppColors.primaryColor),
-                                    borderRadius:
-                                    BorderRadius.circular(20),
-                                  ),
-                                  child:  Center(
-                                    child: GText(
-                                      color: index == cubit.buttonSelected ? AppColors.whiteColor : AppColors.primaryColor,
-                                      content: buttonFavouriteTitles[index],
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                )
-                            ),
-                          );
-                        }),
-                      )),
+          padding: EdgeInsets.symmetric(horizontal: 0.04.w, vertical: 0.02.h),
+          child: CustomScrollView(
+            slivers: [
+              const SliverToBoxAdapter(child: SizedBox(height: 16)),
+              _buildHorizontalButtonList(cubit),
+              const SliverToBoxAdapter(child: SizedBox(height: 16)),
+              _buildFavouriteItemList(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildHorizontalButtonList(FavouriteItemsCubit cubit) {
+    return SliverToBoxAdapter(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(buttonFavouriteTitles.length, (index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: GestureDetector(
+                onTap: () => cubit.selectButton(index),
+                child: Container(
+                  width: 0.42.w,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: BoxDecoration(
+                    color: index == cubit.buttonSelected
+                        ? AppColors.primaryColor
+                        : AppColors.secondaryColor,
+                    border: Border.all(color: AppColors.primaryColor),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Center(
+                    child: GText(
+                      color: index == cubit.buttonSelected
+                          ? AppColors.whiteColor
+                          : AppColors.primaryColor,
+                      content: buttonFavouriteTitles[index],
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ),
-                const SliverToBoxAdapter(
-                    child: SizedBox(
-                  height: 16,
-                )),
-                SliverList.separated(
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return ReusableItemCard(
-                      imagePath: AppImages.tuberVaccineTest,
-                      title: AppText.sideEffectsTuberculosisVaccine,
-                      description: "فعال بنسبة99%",
-                      subDescription: "يتم اخده مره واحده",
-                      onPressedIconFavourite: () {},
-                      onTapCard: () {},
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const SizedBox(
-                      height: 8,
-                    );
-                  },
-                )
-              ],
-            ));
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFavouriteItemList() {
+    return SliverList.separated(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return ReusableItemCard(
+          imagePath: AppImages.tuberVaccineTest,
+          title: AppText.sideEffectsTuberculosisVaccine,
+          description: "فعال بنسبة99%",
+          subDescription: "يتم اخده مره واحده",
+          onPressedIconFavourite: () {},
+          onTapCard: () {},
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const SizedBox(height: 8);
       },
     );
   }
 }
 
 
+
 List<String> buttonFavouriteTitles=[
   AppText.articles,
   AppText.doctors,
-
 ];
 
 /*
