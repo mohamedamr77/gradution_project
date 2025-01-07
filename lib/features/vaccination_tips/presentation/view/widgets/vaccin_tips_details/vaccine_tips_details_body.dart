f6a9bb8e14c495c62edf8c245d367fc5be92a759
mohamedamr77 <moamr947@gmail.com>
2025-01-07 04:37:28 +0200

    refecator code

--- lib/features/vaccination_tips/presentation/view/widgets/vaccin_tips_details/vaccine_tips_details_body.dart
+++ lib/features/vaccination_tips/presentation/view/widgets/vaccin_tips_details/vaccine_tips_details_body.dart
@@ -11,84 +11,68 @@ import '../../../../data/model/tips_after_vaccine_list.dart';
 
 class VaccineTipsDetailsBody extends StatelessWidget {
   const VaccineTipsDetailsBody({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 0.04.w, vertical: 0.02.h),
       child: CustomScrollView(
         slivers: [
-          SliverToBoxAdapter(
-            child: ReusableItemCard(
-              imagePath: AppImages.tuberVaccineTest,
-              title: AppText.rotavirusVaccine,
-              description: "فعال بنسبة99%",
-              subDescription: "يتم اخده مره واحده",
-              onPressedIconFavourite: () {},
-              onTapCard: () {},
-              isDetails: true,
-            ),
-          ),
-          const SliverToBoxAdapter(
-            child: SizedBox(
-              height: 24,
-            ),
-          ),
-          const SliverToBoxAdapter(
-              child: CustomTitleText(title: AppText.beforeVaccination)),
-          const SliverToBoxAdapter(
-            child: SizedBox(
-              height: 16,
-            ),
-          ),
-          SliverList.separated(
-            itemBuilder: (context, index) {
-              return Expanded(
-                  child: GText(
-                color: AppColors.mediumGrayColor,
-                content: "${index + 1} . ${tipsBeforeVaccineList[index]}",
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-              ));
-            },
-            separatorBuilder: (context, index) {
-              return const SizedBox(
-                height: 16,
-              );
-            },
-            itemCount: tipsBeforeVaccineList.length,
-          ),
-          const SliverToBoxAdapter(
-            child: SizedBox(
-              height: 24,
-            ),
-          ),
-          const SliverToBoxAdapter(
-              child: CustomTitleText(title: AppText.postVaccination)),
-          const SliverToBoxAdapter(
-            child: SizedBox(
-              height: 16,
-            ),
-          ),
-          SliverList.separated(
-            itemBuilder: (context, index) {
-              return Expanded(
-                  child: GText(
-                    color: AppColors.mediumGrayColor,
-                    content: "${index + 1} . ${tipsAfterVaccineList[index]}",
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  ));
-            },
-            separatorBuilder: (context, index) {
-              return const SizedBox(
-                height: 16,
-              );
-            },
-            itemCount: tipsAfterVaccineList.length,
-          ),
+          _buildReusableItemCard(),
+          _buildSpacer(height: 24),
+          _buildSectionTitle(AppText.beforeVaccination),
+          _buildSpacer(height: 16),
+          _buildTipsList(tipsBeforeVaccineList),
+          _buildSpacer(height: 24),
+          _buildSectionTitle(AppText.postVaccination),
+          _buildSpacer(height: 16),
+          _buildTipsList(tipsAfterVaccineList),
         ],
       ),
+    );
+  }
+
+  SliverToBoxAdapter _buildReusableItemCard() {
+    return SliverToBoxAdapter(
+      child: ReusableItemCard(
+        imagePath: AppImages.tuberVaccineTest,
+        title: AppText.rotavirusVaccine,
+        description: "فعال بنسبة99%",
+        subDescription: "يتم اخده مره واحده",
+        onPressedIconFavourite: () {},
+        onTapCard: () {},
+        isDetails: true,
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildSpacer({required double height}) {
+    return SliverToBoxAdapter(
+      child: SizedBox(
+        height: height,
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildSectionTitle(String title) {
+    return SliverToBoxAdapter(
+      child: CustomTitleText(title: title),
+    );
+  }
+
+  SliverList _buildTipsList(List<String> tipsList) {
+    return SliverList.separated(
+      itemBuilder: (context, index) {
+        return GText(
+          color: AppColors.mediumGrayColor,
+          content: "${index + 1} . ${tipsList[index]}",
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+        );
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(height: 16);
+      },
+      itemCount: tipsList.length,
     );
   }
 }
