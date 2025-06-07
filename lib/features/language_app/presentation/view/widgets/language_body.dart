31758fadf011d8a369aa7ff0835d37303bd13ef1
mohamedamr77 <moamr947@gmail.com>
2025-06-07 22:43:04 +0300

    solve issue not change language

--- lib/features/language_app/presentation/view/widgets/language_body.dart
+++ lib/features/language_app/presentation/view/widgets/language_body.dart
@@ -1,29 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gradutionproject/core/navigation/navigation_manager.dart';
 import 'package:gradutionproject/core/shared_widget/custom_elevated_btn.dart';
 import 'package:gradutionproject/core/utils/locale_keys.g.dart';
+import 'package:gradutionproject/features/bottom_nav_bar/presentation/view/bottom_nav_bar_screen.dart';
 import '../../../../../core/shared_widget/custom_continer_check.dart';
 
-class LanguageBody extends StatefulWidget {
+class LanguageBody extends StatelessWidget {
   const LanguageBody({super.key});
 
   @override
-  State<LanguageBody> createState() => _LanguageBodyState();
-}
-
-class _LanguageBodyState extends State<LanguageBody> {
-  @override
   Widget build(BuildContext context) {
-    // Get the current locale
-    final isArabic = context.locale.languageCode == 'ar';
+    final currentLocale = context.locale;
 
     return Center(
       child: Column(
         children: [
           const Spacer(flex: 3),
           CustomContainerCheck(
-            isCheck: isArabic, // Check if current language is Arabic
+            isCheck :currentLocale.languageCode == 'ar', // Check if current language is Arabic
             title: LocaleKeys.arabicLanguage.tr(),
             onTap: () {
               context.setLocale(const Locale('ar'));
@@ -31,7 +27,7 @@ class _LanguageBodyState extends State<LanguageBody> {
           ),
           const Spacer(),
           CustomContainerCheck(
-            isCheck: !isArabic, // Check if current language is English
+            isCheck: currentLocale.languageCode == 'en', // Check if current language is English
             title: LocaleKeys.englishLanguage.tr(),
             onTap: () {
               context.setLocale(const Locale('en'));
@@ -40,8 +36,8 @@ class _LanguageBodyState extends State<LanguageBody> {
           const Spacer(flex: 3),
           CustomElevatedButton(
             onPress: () {
-              // Toggle language between Arabic and English
-              context.setLocale(isArabic ? const Locale('en') : const Locale('ar'));
+              context.setLocale(currentLocale.languageCode == 'ar' ? const Locale('en') : const Locale('ar'));
+              NavigationManager.push(BottomNavBarScreen.id);
             },
             titleButton: LocaleKeys.change.tr(),
           ),
