ed040f32ac0f6f30f7d9c1fc0b729352f966467a
mohamedamr77 <moamr947@gmail.com>
2025-06-08 15:52:35 +0300

    32

--- lib/core/shared_widget/custom_list_tile.dart
+++ lib/core/shared_widget/custom_list_tile.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gradutionproject/core/shared_widget/global_text.dart';
@@ -21,17 +22,19 @@ class CustomListTile extends StatelessWidget {
       color: const Color(0xffF4F0F0),
       child: ListTile(
         onTap: onTap,
-        title: GText(
-          color: colorTitle ?? AppColors.darkGrayColor,
-          content: title,
-          fontSize: 18,
-        ),
-        trailing: Transform.translate(
-          offset: const Offset(-12, 0),
-          child: Icon(
-            Icons.arrow_forward_ios,
-            color: AppColors.primaryColor.withOpacity(0.6),
+        title: FittedBox(
+          fit: BoxFit.scaleDown,
+          alignment:context.locale.languageCode == 'ar' ?
+          Alignment.centerRight : Alignment.centerLeft,
+          child: GText(
+            color: colorTitle ?? AppColors.darkGrayColor,
+            content: title,
+            fontSize: 18,
           ),
+        ),
+        trailing: Icon(
+          Icons.arrow_forward_ios,
+          color: AppColors.primaryColor.withOpacity(0.6),
         ),
         leading: SvgPicture.asset(imagePath),
       ),
--- lib/features/medicines/presentation/view/widgets/medicines_body.dart
+++ lib/features/medicines/presentation/view/widgets/medicines_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gradutionproject/core/shared_widget/custom_text_form_field.dart';
 import 'package:gradutionproject/core/utils/extentions/screen_size.dart';
 import 'package:gradutionproject/features/medicines/presentation/viewModel/medicines_cubit.dart';
 import 'package:gradutionproject/features/medicines/presentation/viewModel/medicines_state.dart';
@@ -25,6 +26,12 @@ class MedicinesBody extends StatelessWidget {
                             horizontal: 0.04.w, vertical: 0.02.h),
                         child:  CustomScrollView(
                           slivers: [
+                            SliverToBoxAdapter(child: CustomTextField(hintText: "ابحث هنا ......", title: "",
+                             onChanged: (p0) {
+                              BlocProvider.of<MedicinesCubit>(context).getAllMedicines(search: p0);
+                             },
+                            )),
+                             SliverToBoxAdapter(child: SizedBox(height: 20,),),
                             MedicinesListItems(medications:state.medicineResponse.data?.rows??[],),
                           ],
                         ),
