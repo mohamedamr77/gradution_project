42e579da1cc1f31caa0e6f2ff5c942caf9e3c1e5
mohamedamr77 <moamr947@gmail.com>
2025-06-19 01:18:36 +0300

    make loading state design

--- lib/features/child_information/presentation/view/child_info_screen.dart
+++ lib/features/child_information/presentation/view/child_info_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gradutionproject/core/helper/api_service.dart';
 import 'package:gradutionproject/features/child_information/presentation/view/widgets/child_info_body.dart';
+import 'package:gradutionproject/features/child_information/presentation/view_model/child_information_state.dart';
 import 'package:gradutionproject/features/child_information/presentation/view_model/child_informtion_cubit.dart';
 
+import '../../../../core/utils/app_colors.dart';
 import '../../data/repo/child_info_impl.dart';
 
 class ChildInfoScreen extends StatelessWidget {
@@ -15,8 +17,26 @@ class ChildInfoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => ChildInformationCubit(ChildInfoImpl(apiService: ApiService())),
-      child: const Scaffold(
-        body: ChildInfoBody(),
+      child:  Stack(
+        children: [
+          const Scaffold(
+            body: ChildInfoBody(),
+          ),
+          BlocBuilder<ChildInformationCubit,ChildInformationState>(
+            builder: (context, state) {
+              if (state is ChildInformationLoadingState) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.black12,
+                  child: const CircularProgressIndicator(
+                    color:  AppColors.primaryColor,
+                  ),
+                );
+              }else {
+                return const SizedBox();
+              }
+            },)
+        ],
       ),
     );
   }
