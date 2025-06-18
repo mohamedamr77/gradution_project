e229062178b45a5af084e4ec20858f4d1d898d97
mohamedamr77 <moamr947@gmail.com>
2025-06-18 18:53:11 +0300

    add skel

--- lib/features/vaccine_times/presentation/view/widgets/vaccine_times_body.dart
+++ lib/features/vaccine_times/presentation/view/widgets/vaccine_times_body.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gradutionproject/features/vaccine_times/presentation/view/widgets/vaccine_times_buttons.dart';
 import 'package:gradutionproject/features/vaccine_times/presentation/view/widgets/vaccine_times_list_items.dart';
+import 'package:gradutionproject/features/vaccine_times/presentation/viewModel/vaccine_cubit/vaccine_cubit.dart';
+import 'package:gradutionproject/features/vaccine_times/presentation/viewModel/vaccine_cubit/vaccine_state.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 import '../../../../../core/shared_widget/header_home_section.dart';
 
@@ -9,26 +13,32 @@ class VaccineTimesBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(
+    return Padding(
+      padding: const EdgeInsets.only(
         top: 16,
         left: 16,
         right: 16,
       ),
-      child: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(child: HeaderBottomNavBarScreen()),
-          SliverToBoxAdapter(
-              child: SizedBox(
-            height: 16,
-          )),
-          VaccineTimesButtons(),
-          SliverToBoxAdapter(
-              child: SizedBox(
-            height: 16,
-          )),
-          VaccineTimesListItems(),
-        ],
+      child: BlocBuilder<VaccineCubit, VaccineState>(
+        builder: (context, state) {
+          return Skeletonizer(
+              enabled: state is VaccineLoadingState,
+              child: const CustomScrollView(
+                slivers: [
+                  SliverToBoxAdapter(child: HeaderBottomNavBarScreen()),
+                  SliverToBoxAdapter(
+                      child: SizedBox(
+                        height: 16,
+                      )),
+                  VaccineTimesButtons(),
+                  SliverToBoxAdapter(
+                      child: SizedBox(
+                        height: 16,
+                      )),
+                  VaccineTimesListItems(),
+                ],
+              ) );
+        },
       ),
     );
   }
