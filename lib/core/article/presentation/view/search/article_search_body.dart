1b1932bdcf572e28df2bbb344e005101fb25bf53
mohamedamr77 <moamr947@gmail.com>
2025-06-18 14:50:38 +0300

    search field

--- lib/core/article/presentation/view/search/article_search_body.dart
+++ lib/core/article/presentation/view/search/article_search_body.dart
@@ -1,12 +1,24 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:gradutionproject/core/shared_widget/custom_text_form_field.dart';
+import 'package:gradutionproject/core/utils/locale_keys.g.dart';
 
 class ArticleSearchBody extends StatelessWidget {
   const ArticleSearchBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: CustomTextField(hintText: LocaleKeys.searchHere.tr(),
+                title:""),
+          ),
 
+        ],
+      ),
     );
   }
 }
