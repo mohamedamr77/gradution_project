b3431cd183ea572e39e33e043f87c3e1aa2b60cb
mohamedamr77 <moamr947@gmail.com>
2025-06-19 03:27:11 +0300

    make search

--- lib/features/vaccination_tips/presentation/view/vaccination_tips_screen.dart
+++ lib/features/vaccination_tips/presentation/view/vaccination_tips_screen.dart
@@ -5,6 +5,8 @@ import 'package:gradutionproject/core/shared_widget/custom_app_bar.dart';
 import 'package:gradutionproject/core/utils/app_icons.dart';
 import 'package:gradutionproject/features/vaccination_tips/presentation/view/widgets/vaccination_tips_body.dart';
 import 'package:easy_localization/easy_localization.dart';
+import '../../../../core/article/presentation/view/search/article_search_screen.dart';
+import '../../../../core/navigation/navigation_manager.dart';
 import '../../../../core/utils/locale_keys.g.dart';
 
 import '../viewModel/vaccination_tips_cubit.dart';
@@ -21,9 +23,14 @@ class VaccinationTipsScreen extends StatelessWidget {
       child: Scaffold(
         appBar: CustomAppBar(
           actions: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: SvgPicture.asset(AppIcons.search),
+            InkWell(
+              onTap: () {
+                NavigationManager.push(ArticleSearchScreen.id);
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: SvgPicture.asset(AppIcons.search),
+              ),
             )
           ],
           title: LocaleKeys.tipsBeforeAfterVaccination.tr(),
--- lib/features/vaccine_info/presentation/view/vaccine_info_screen.dart
+++ lib/features/vaccine_info/presentation/view/vaccine_info_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter_svg/svg.dart';
 import 'package:gradutionproject/features/vaccine_info/presentation/view/widgets/vaccine_info_body.dart';
 import 'package:gradutionproject/features/vaccine_info/presentation/viewModel/vaccine_info_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
+import '../../../../core/article/presentation/view/search/article_search_screen.dart';
+import '../../../../core/navigation/navigation_manager.dart';
 import '../../../../core/utils/locale_keys.g.dart';
 
 import '../../../../core/shared_widget/custom_app_bar.dart';
@@ -21,9 +23,14 @@ class VaccineInfoScreen extends StatelessWidget {
       child: Scaffold(
         appBar: CustomAppBar(
           actions: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: SvgPicture.asset(AppIcons.search),
+            InkWell(
+              onTap: () {
+                NavigationManager.push(ArticleSearchScreen.id);
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: SvgPicture.asset(AppIcons.search),
+              ),
             )
           ],
           title: LocaleKeys.generalArticlesVaccinations.tr(),
