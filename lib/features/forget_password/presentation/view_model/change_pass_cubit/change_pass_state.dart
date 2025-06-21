cd6d6ebe9e3db72ae8194d8db9d790607d7f7cba
mohamedamr77 <moamr947@gmail.com>
2025-06-22 01:57:25 +0300

    make state forget password

--- lib/features/forget_password/presentation/view_model/change_pass_cubit/change_pass_state.dart
+++ lib/features/forget_password/presentation/view_model/change_pass_cubit/change_pass_state.dart
@@ -1,3 +1,17 @@
+import 'package:gradutionproject/features/forget_password/data/model/forget_password/forget_password_response.dart';
+
 abstract class ChangePassState {}
 
 class ChangePassInitialState extends ChangePassState {}
+
+class ForgetPasswordLoadingState extends ChangePassState {}
+
+class ForgetPasswordFaliureState extends ChangePassState {}
+
+class ForgetPasswordSuccessState extends ChangePassState {
+  final ForgetPasswordResponse forgetPasswordResponse;
+
+  ForgetPasswordSuccessState({required this.forgetPasswordResponse});
+}
+
+class ConfirmResetPasswordLoadingState extends ChangePassState {}
